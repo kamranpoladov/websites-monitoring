1e8b83c5c0e98911119ddc458b2202634303bbfc
Kamran Poladov <kamranpoladov1803@gmail.com>
2021-10-28 13:44:48 +0200

    cross-platform terminal opener

--- src/Utils/open-terminal.util.ts
+++ src/Utils/open-terminal.util.ts
@@ -3,20 +3,44 @@ import { execSync } from 'child_process';
 export const openTerminal = (command: string) => {
   switch (process.platform) {
     case 'linux':
-      execSync(`gnome-terminal -e "${command}"`, {
-        stdio: 'ignore'
-      });
+      try {
+        execSync(`gnome-terminal -e "${command}"`, {
+          stdio: 'ignore'
+        });
+      } catch (e) {
+        console.log(
+          'Sorry, your terminal application is currently not supported'
+        );
+        process.exit();
+      }
       break;
     case 'darwin':
-      execSync(`osascript -e "tell app "Terminal" to do script "${command}""`, {
-        stdio: 'ignore'
-      });
+      try {
+        execSync(
+          `osascript -e "tell app "Terminal" to do script "${command}""`,
+          {
+            stdio: 'ignore'
+          }
+        );
+      } catch (e) {
+        console.log(
+          'Sorry, your terminal application is currently not supported'
+        );
+        process.exit();
+      }
       break;
     case 'win32':
-      execSync(`start cmd.exe /K ${command}`, { stdio: 'ignore' });
+      try {
+        execSync(`start cmd.exe /K ${command}`, { stdio: 'ignore' });
+      } catch (e) {
+        console.log(
+          'Sorry, your terminal application is currently not supported'
+        );
+        process.exit();
+      }
       break;
     default:
-      console.log('Sorry, your terminal is currently not supported :(');
+      console.log('Sorry, your operating system is currently not supported :(');
       process.exit();
   }
 };
